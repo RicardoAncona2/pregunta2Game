8c033782ac8d2d6e5965f922cd4d05acefaad4a7
Adrian Navarro <adrian_navarro97@hotmail.com>
2021-04-12 18:46:39 -0500

    fix/se agregan imagenes de Victoria y Derrota

--- src/components/YouLost.vue
+++ src/components/YouLost.vue
@@ -5,7 +5,9 @@
        
       <h3 class="title is-3 has-text-centered">¡HAS PERDIDO! <br> 
       <br> TU PUNTAJE ES:</h3>
-      <v-img src="@/assets/trofeo.png"></v-img>
+      <figure class="image is-128x128">
+  <img src="https://www.flaticon.es/svg/vstatic/svg/75/75454.svg?token=exp=1618271780~hmac=d580a44217ea8b38b6f12911db4ff271">
+</figure>
       <br>
    </div>
     <br>
@@ -22,8 +24,13 @@
 h3.title {
   font-family: Avenir, Arial, sans-serif;
   color: white;
-  
-  
+   
+}
+
+figure.is-128x128 {
+   display: block;
+    margin-left: auto;
+    margin-right: auto;
 }
 
 </style>
--- src/components/YouWon.vue
+++ src/components/YouWon.vue
@@ -5,7 +5,9 @@
        
       <h3 class="title is-3 has-text-centered">¡HAS GANADO! <br> 
       <br> TU PUNTAJE ES:</h3>
-      <v-img src="@/assets/trofeo.png"></v-img>
+        <figure class="image is-128x128">
+  <img src="https://www.flaticon.es/svg/vstatic/svg/912/912233.svg?token=exp=1618270252~hmac=b33f1ee6a7c983065dd8c4ffed052904">
+</figure>
       <br>
    </div>
     <br>
@@ -23,7 +25,12 @@ h3.title {
   font-family: Avenir, Arial, sans-serif;
   color: white;
   
-  
+}
+
+figure.is-128x128 {
+   display: block;
+    margin-left: auto;
+    margin-right: auto;
 }
 
 </style>
